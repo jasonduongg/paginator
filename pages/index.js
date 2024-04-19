00d3340a07e73f7b99994149a51bf535cce78444
jasonduong-portfolio <jasonduong.website@gmail.com>
2024-04-19 10:56:47 -0700

    logigc finished need final styling

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,76 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
+import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   const [numItems, setNumItems] = useState(10);
   const [currentPage, setCurrentPage] = useState(1);
   const data = {
-    1: {date: "22.07.21", time: "16.02pm", file: "report 1"},
-    2: {date: "23.07.21", time: "12.02pm", file: "report 2"},
-    3: {date: "12.01.21", time: "24.00pm", file: "report 3"},
-    4: {date: "22.07.21", time: "06.02pm", file: "report 4"}
   }
+
+  const renderDataForCurrentPage = () => {
+    const startIndex = (currentPage - 1) * numItems;
+    const endIndex = Math.min(startIndex + numItems, numDataEntries);
+    const renderedData = [];
+
+    for (let i = startIndex + 1; i <= endIndex; i++) {
+      const item = data[i];
+      renderedData.push(
+        <div>
+          <div className={styles.data_row} key={i}>
+            <div className={styles.meta_container}>
+              <p className={styles.date}>{item.date}</p>
+              <p className={styles.time}>{item.time}</p>
+            </div>
+            <div className={styles.report_container}>
+              <p className={styles.report}>{item.file}</p>
+            </div>
+            <div className={styles.download_container}>
+              <button className={styles.downloadButton}>Download</button>
+            </div>
+          </div>
+        </div>
+       
+        
+      );
+    }
+
+    return renderedData;
+  };
+
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  
+  function getRandomDate() {
+    const year = getRandomInt(2000, 2022);
+    const month = getRandomInt(1, 12);
+    const day = getRandomInt(1, 28); 
+    return `${day < 10 ? '0' + day : day}.${month < 10 ? '0' + month : month}.${year}`;
+  }
+  
+  function getRandomTime() {
+    const hours = getRandomInt(0, 23);
+    const minutes = getRandomInt(0, 59);
+    const ampm = hours < 12 ? 'AM' : 'PM';
+    const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
+    return `${formattedHours}:${minutes < 10 ? '0' + minutes : minutes} ${ampm}`;
+  }
+
+  let index = Object.keys(data).length + 1;
+  while (index <= 24) {
+    const randomDate = getRandomDate();
+    const randomTime = getRandomTime();
+    data[index] = {
+      date: randomDate,
+      time: randomTime,
+      file: `report ${index}`
+    };
+    index++;
+  }
+
   const numDataEntries = Object.keys(data).length;
   const numPages = Math.ceil(numDataEntries / numItems);
 
@@ -24,6 +84,9 @@ export default function Home() {
   }
 
   return (
+    <div className='body'>
+
+  
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
@@ -39,8 +102,24 @@ export default function Home() {
             </div>
 
             <div className={styles.nav_buttons}>
-              <button className={styles.nav_button}>F</button>
-              <button className={styles.nav_button}>X</button>
+              <button className={styles.nav_button}>
+              <Image
+                className='button-icon'
+                src="/icons/Filter.png" 
+                width={15}
+                height={15}
+                
+              />
+              </button>
+              <button className={styles.nav_button}>
+              <Image
+                className='button-icon'
+                src="/icons/Close.png" 
+                width={15}
+                height={15}
+                
+              />
+              </button>
             </div>
           </div>
 
@@ -56,19 +135,27 @@ export default function Home() {
             </div>
           </div>
 
-          <div className={styles.pg_main}></div>
+          <div className={styles.pg_main}>
+            <div className={styles.data_container}>
+              {renderDataForCurrentPage()}
+            </div>
+          </div>
 
           <div className={styles.pg_footer}>
-            {Array.from({ length: numPages }, (_, index) => (
-              <button
-                key={index + 1}
-                className={styles.page_button}
-                onClick={() => setPage(index + 1)}
-                disabled={currentPage === index + 1}
-              >
-                {index + 1}
-              </button>
-            ))}
+            <div className={styles.page_button_container}>
+              <button onClick={() => setPage(currentPage - 1)}> Prev </button>
+                {Array.from({ length: numPages }, (_, index) => (
+                  <button
+                    key={index + 1}
+                    className={styles.page_button}
+                    onClick={() => setPage(index + 1)}
+                    disabled={currentPage === index + 1}
+                  >
+                    {index + 1}
+                  </button>
+              ))} 
+              <button onClick={() => setPage(currentPage + 1)}> Next </button>
+            </div>
             <select value={numItems} onChange={handleNumItemsChange} className={styles.dropdown}>
               <option value={5}>5</option>
               <option value={10}>10</option>
@@ -77,6 +164,7 @@ export default function Home() {
           </div>
         </div>
       </main>
+    </div>
     </div>
   );
 }
